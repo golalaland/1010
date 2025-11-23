11583dbda645fc586231c8a25c726333b1ccc61a
golalaland <golalaland@proton.me>
2025-11-23 14:10:05 +0100

    Update tcr.js

--- tcr.js
+++ tcr.js
@@ -520,14 +520,14 @@ function showGiftAlert(text) {
 /* ---------- Redeem Link ---------- */
 function updateRedeemLink() {
   if (!refs.redeemBtn || !currentUser) return;
-  refs.redeemBtn.href = `https://golalaland.github.io/crdb/shop.html?uid=${encodeURIComponent(currentUser.uid)}`;
+  refs.redeemBtn.href = `menu.html?uid=${encodeURIComponent(currentUser.uid)}`;
   refs.redeemBtn.style.display = "inline-block";
 }
 
 /* ---------- Tip Link ---------- */
 function updateTipLink() {
   if (!refs.tipBtn || !currentUser) return;
-  refs.tipBtn.href = `https://golalaland.github.io/1010/tapmaster.html?uid=${encodeURIComponent(currentUser.uid)}`;
+  refs.tipBtn.href = `menu.html?uid=${encodeURIComponent(currentUser.uid)}`;
   refs.tipBtn.style.display = "inline-block";
 }
 
